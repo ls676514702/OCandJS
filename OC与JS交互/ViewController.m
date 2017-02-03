2889c34d0fa5b9798c5b496240e16f07441c2282
梁森 <liangsen@liangsendeMacBook-Pro.local>
2017-02-03 08:42:00 +0800

    OC和JS交互

--- OC与JS交互/ViewController.m
+++ OC与JS交互/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import <SDWebImage/SDWebImageDownloader.h>
+@interface ViewController ()<UIWebViewDelegate>
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -16,9 +17,40 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"123");
     // Do any additional setup after loading the view, typically from a nib.
+    self.webView.delegate = self;
+    NSURL *url = [NSURL URLWithString:@"http://m.dianping.com/tuan/deal/15379196"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    [webView stringByEvaluatingJavaScriptFromString:
+          @"var figure = document.getElementsByTagName('figure')[0];"
+           "var imgSrc = figure.children[0]; "
+           "imgSrc.onclick = function(){window.location.href='aiyou://src='+this.src;};"];
+    
 }
 
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    if ([request.URL.scheme isEqualToString:@"aiyou"]) {
+        NSString *str = request.URL.absoluteString;
+        NSRange range = [str rangeOfString:@"aiyou://src="];
+        NSString *subStr = [str substringFromIndex:range.location+range.length];
+        SDWebImageDownloader *downLoader = [[SDWebImageDownloader alloc] init];
+        [downLoader downloadImageWithURL:[NSURL URLWithString:subStr] options:SDWebImageDownloaderProgressiveDownload progress:^(NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL) {
+            NSLog(@"%ld",receivedSize/expectedSize);
+        } completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, BOOL finished) {
+            UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+        }];
+        return NO;
+    }
+    
+    return YES;
+}
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo{
+    NSLog(@"完成");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
